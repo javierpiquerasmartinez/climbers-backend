f093f088f113ad21494cd8f339cd839536da4b85
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2025-03-29 12:11:49 +0100

    Eliminar la extensión .js de la importación en userRoutes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createUser, updateUser, getUsersWithFilters } from '../controllers/userController.js';
+import { createUser, updateUser, getUsersWithFilters } from '../controllers/userController';
 
 const router = Router();
 
