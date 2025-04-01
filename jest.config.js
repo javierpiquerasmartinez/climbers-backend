5afc6b93fc4207e3a3c510cd9106802d916cff1e
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2025-04-01 08:55:33 +0200

    Actualizar configuración de Jest y TypeScript: agregar soporte para módulos ESM y comando de limpieza

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  testEnvironment: "node",
   transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
+    "^.+\\.tsx?$": "ts-jest"
   },
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1"
+  }
 };
