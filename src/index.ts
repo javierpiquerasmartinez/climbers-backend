82d4ea505506042e4b5ff44a90a079d26c248fb8
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2025-04-08 17:25:09 +0200

    Mejorar el registro de logs: incluir el modo NODE_ENV en el mensaje del servidor y ajustar la configuración del logger para desarrollo y producción

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import { logger } from "./utils/logger.js";
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  logger.info(`Servidor corriendo en http://localhost:${PORT}`);
+  logger.info(`Servidor corriendo en http://localhost:${PORT} en ${process.env.NODE_ENV} mode`);
 });
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,27 @@
-
 import pino from 'pino';
 
-export const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:standard',
-      ignore: 'pid,hostname',
-    },
-  },
-});
+const isDev = process.env.NODE_ENV === 'development';
+
+export const logger = pino(
+  isDev
+    ? {
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard',
+          ignore: 'pid,hostname',
+          levelFirst: true,
+        },
+      },
+    }
+    : {
+      // Producción: salida JSON lista para logs externos
+      formatters: {
+        level(label) {
+          return { level: label };
+        },
+      },
+      timestamp: pino.stdTimeFunctions.isoTime,
+    }
+);
