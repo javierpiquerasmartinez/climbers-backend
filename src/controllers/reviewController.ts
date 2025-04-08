ccafc11928abed605ddb28b71ea036794f699d97
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2025-04-08 14:26:19 +0200

    Refactor ReviewController: eliminar importación innecesaria de prisma y optimizar la estructura del código

--- src/controllers/reviewController.ts
+++ src/controllers/reviewController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import prisma from '../prisma/client.js';
 import { ReviewService } from '../services/reviewService.js';
 
 export const createReview = async (req: Request, res: Response) => {
