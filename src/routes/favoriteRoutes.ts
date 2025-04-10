6a05936925286d75477a0beece12d8a6c93f3ece
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2025-04-10 11:21:35 +0200

    Agregar rutas para manejar favoritos: obtener, agregar y eliminar favoritos

--- src/routes/favoriteRoutes.ts
+++ src/routes/favoriteRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { verifyGoogleToken } from '../middlewares/verifyGoogleToken.js';
 import { addFavorite, getFavorites, removeFavorite } from '../controllers/favoriteController.js';
-import { validateBody, validateParams } from '../middlewares/validate.js';
+import { validateParams } from '../middlewares/validate.js';
 import { favoriteSchema } from '../validators/favoriteSchema.js';
+import { catchAsync } from '../utils/catchAsync.js';
 
 const router = Router();
 
-router.get('/', verifyGoogleToken, getFavorites);
+router.get('/', verifyGoogleToken, catchAsync(getFavorites));
 router.post('/:favoriteId', validateParams(favoriteSchema), verifyGoogleToken, addFavorite);
 router.delete('/:favoriteId', validateParams(favoriteSchema), removeFavorite);
 
